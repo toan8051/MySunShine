43758c9d4df44662eaaa510d3d13a2ae21fe9f15
le toan <toan8051@gmail.com>
2015-07-24 17:13:03 +0700

    Tao và thêm fragment

--- app/src/main/java/vn/alovoice/sunshine/MainActivity.java
+++ app/src/main/java/vn/alovoice/sunshine/MainActivity.java
@@ -65,6 +65,7 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
             getSupportActionBar().setElevation(0f);
         }
 
+        //T?o và n?p Fragment cho forecast
         ForecastFragment forecastFragment =  ((ForecastFragment)getSupportFragmentManager()
                 .findFragmentById(R.id.fragment_forecast));
         forecastFragment.setUseTodayLayout(!mTwoPane);
